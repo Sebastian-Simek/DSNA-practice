9bdacb2a7d814fa91817b8bcdf335d2e2670ab69
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-08-09 10:35:50 -0700

    added some work

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-class Stack {}
+class Stack {
+    #list = [];
+
+    constructor(newList) {
+        if (newList) this.#list = newList;
+    }
+
+    push(newList) {
+        this.#list.push(newList);
+    }
+
+    peek(newList) {
+        this.#list.peek(newList)
+    }
+
+    get count() {
+        return this.#list.length;
+    }
+
+    set count(count) {
+        return (this.#list.length = count);
+    }
+
+}
+
+const ourList = new Stack([1, 2, 3]);
+console.log(ourList);
+ourList.push(4);
 
 class Queue {}
 
--- index.test.js
+++ index.test.js
@@ -5,5 +5,8 @@ describe("Stack Class", () => {
     stack.push(1);
     expect(stack.peek()).toEqual(1);
   });
-  // add more tests here...
+  it.skip('pop should remove the last item in the stack', () => {
+    const stack = new Stack();
+    expect(stack.pop()).toBe(stack.length - 1);
+  });
 });
